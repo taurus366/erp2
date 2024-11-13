cb5b866e50ef43cbaee3986a484438b72cfa020d
taurus <taurus.ali47@gmail.com>
2024-11-13 18:52:37 +0200

    .

--- libs/cl_base/ui/feature/src/lib/ui/ui.component.ts
+++ libs/cl_base/ui/feature/src/lib/ui/ui.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { MenuItem, UiService } from './ui.service';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'lib-ui',
@@ -10,10 +11,11 @@ import { MenuItem, UiService } from './ui.service';
   templateUrl: './ui.component.html',
   styleUrls: ['./ui.component.css'], // Fixed to `styleUrls`
 })
-export class UiComponent {
+export class UiComponent implements OnInit {
   menuItems: MenuItem[] = [];
+  readonly browserTitle = 'client';
 
-  constructor(private uiService: UiService) {}
+  constructor(private uiService: UiService, private title: Title) {}
 
   ngOnInit(): void {
     // Define and add menu items with children and components
@@ -29,5 +31,12 @@ export class UiComponent {
 
     // Retrieve the menu items for display
     this.menuItems = this.uiService.getMenuItems();
+
+    this.changePageTitle(this.browserTitle);
   }
+
+  changePageTitle(title: string) {
+    this.title.setTitle(title);
+  }
+
 }
---
+++ libs/cl_base/ui/feature/src/lib/ui/ui.module.ts
@@ -0,0 +1,43 @@
+// import { NgModule, ErrorHandler } from '@angular/core';
+// import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+// import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+// import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+// import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+// // import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+// import { RouterModule } from '@angular/router';
+//
+// import { AppComponent } from './app.component';
+// // import { ErrorHandlerService } from './shared/services/error-handler.service';
+// // import { TokenInterceptor } from './shared/interceptors/token.interceptor';
+// // import { InMemoryDataService } from './shared/inmemory-db/inmemory-db.service';
+// // import { rootRouterConfig } from './app.routing';
+//
+// // AoT requires an exported function for factories
+// export function HttpLoaderFactory(httpClient: HttpClient) {
+//   return new TranslateHttpLoader(httpClient);
+// }
+//
+// @NgModule({
+//   imports: [
+//     BrowserModule,
+//     HttpClientModule,
+//     TranslateModule.forRoot({
+//       loader: {
+//         provide: TranslateLoader,
+//         useFactory: HttpLoaderFactory,
+//         deps: [HttpClient]
+//       }
+//     }),
+//     InMemoryWebApiModule.forRoot(InMemoryDataService, { passThruUnknownUrl: true }),
+//     RouterModule.forRoot(rootRouterConfig),
+//   ],
+//   providers: [
+//     { provide: ErrorHandler, useClass: ErrorHandlerService },
+//     {
+//       provide: HTTP_INTERCEPTORS,
+//       useClass: TokenInterceptor,
+//       multi: true,
+//     },
+//   ],
+// })
+// export class AppModule {}
